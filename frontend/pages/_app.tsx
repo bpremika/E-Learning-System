11b1befd438e81a00425b2d245e9afc107a0b8b3
Pakin Nuntasukkasem <bruceoio@hotmail.com>
2023-01-10 15:59:38 +0700

    feat: finish navbar

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -15,8 +15,8 @@ export default function App({ Component, pageProps }: AppProps) {
                 <Header />
                 <Component {...pageProps} />
 
+                <Header />
             </MantineProvider>
-            <Header />
         </div>
     );
 }
