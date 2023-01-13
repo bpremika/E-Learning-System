de003b4d9d605d90f3e8f473af0c3d97433dfc63
Pakin Nuntasukkasem <bruceoio@hotmail.com>
2023-01-13 19:06:31 +0700

    feat: finish login

--- frontend/components/register/register.tsx
+++ frontend/components/register/register.tsx
@@ -8,6 +8,7 @@ import {
     Select,
     Image,
     Text,
+    PasswordInput,
 } from "@mantine/core";
 import { useUser } from "../../common/contexts/UserContext";
 import { useForm } from "@mantine/form";
@@ -15,8 +16,14 @@ import { Dropzone, IMAGE_MIME_TYPE, FileWithPath } from "@mantine/dropzone";
 import loginpage from "../../pages/loginpage";
 import { client } from "../../common/axios/axios";
 import { showNotification } from "@mantine/notifications";
-export default function Register() {
+import { ClassNames } from "@emotion/react";
+interface prop {
+    isStudent: boolean;
+}
+export default function Register({ isStudent }: prop) {
     const [opened, setOpened] = useState(false);
+    const labename = isStudent ? "student" : "instructor";
+    const aoan = isStudent ? "a" : "an";
     const form = useForm({
         initialValues: {
             prefix: "",
@@ -52,11 +59,17 @@ export default function Register() {
     const guideURLHandler = async (values: formData) => {
         const formValues = form.values;
         try {
-            const res = await client.post("/user/studentRegister", formValues);
+            const res = await client.post(
+                `/user/${labename}Register`,
+                formValues
+            );
             setOpened(false);
             form.reset();
-        } catch (error) {
-            showNotification({});
+        } catch (error: any) {
+            showNotification({
+                title: "can not create user",
+                message: error.response.data.message,
+            });
         }
     };
 
@@ -65,9 +78,10 @@ export default function Register() {
             <Modal
                 opened={opened}
                 onClose={() => setOpened(false)}
-                title="Create New Course"
+                title={`create a new ${labename} account`}
             >
                 <form
+                    className="p-2 flex flex-col gap-3"
                     onSubmit={form.onSubmit((values) =>
                         guideURLHandler(values)
                     )}
@@ -104,7 +118,7 @@ export default function Register() {
                         withAsterisk
                         {...form.getInputProps("email")}
                     />
-                    <TextInput
+                    <PasswordInput
                         placeholder="password"
                         label="password"
                         withAsterisk
@@ -116,15 +130,16 @@ export default function Register() {
                         withAsterisk
                         {...form.getInputProps("phone_number")}
                     />
+                    <div className="w-[100%] h-[10px] bg-white"></div>
                     <Button type="submit">Create</Button>
                 </form>
             </Modal>
-
-            <Group position="center">
-                <Button onClick={() => setOpened(true)}>
-                    Create New Course
-                </Button>
-            </Group>
+            <div
+                onClick={() => setOpened(true)}
+                className="underline decoration-solid text-center "
+            >
+                Create {aoan} {labename} account
+            </div>
         </>
     );
 }
--- frontend/pages/RegisterPage.tsx
+++
@@ -1,5 +0,0 @@
-import Register from "../components/register/register";
-const RegisterPage = () => {
-    return <Register />;
-};
-export default RegisterPage;
