ceaab0283b2fe45aa5a431b0ed92189aaef61387
Punnawat Lohanuit <punnawat58000@gmail.com>
2023-01-13 09:57:09 +0700

    feat: dashboard

--- frontend/components/common/DashboardTotalCard.tsx
+++ frontend/components/common/DashboardTotalCard.tsx
@@ -7,11 +7,11 @@ interface Data {
 
 export default function DashboardTotalCard(props: Data) {
     return (
-        <>
+        <div className="text-2xl font-bold font-['Montserrat']">
             <div className={styles.card1}>
                 <div className={styles.text1_1}>{props.title}</div>
                 <div className={styles.text1_2}>{props.amount}</div>
             </div>
-        </>
+        </div>
     );
 }
--- frontend/pages/dashboard.tsx
+++ frontend/pages/dashboard.tsx
@@ -1,12 +1,147 @@
+import DashboardTotalCard from "../components/common/DashboardTotalCard";
+import DashboardCourseCard from "../components/common/DashboardCourseCard";
 import NavBar from "../components/NavBar";
+import { createStyles, Chip, NativeSelect} from '@mantine/core';
+import { useEffect, useState } from "react";
 
-export default function dashboard() {
+const useStyles = createStyles((theme, _params, getRef) => ({
+  label: {
+    '&[data-checked]': {
+      '&, &:hover': {
+        backgroundColor: theme.colors.blue[theme.fn.primaryShade()],
+        color: theme.white,
+      },
+
+      [`& .${getRef('iconWrapper')}`]: {
+        color: theme.white,
+      },
+    },
+  },
+
+  iconWrapper: {
+    ref: getRef('iconWrapper'),
+  },
+}));
+
+interface Subject{
+    title: string;
+    studentsAmount: number;
+    allStudents: number;
+    image: string;
+}
+interface DashboardProp{
+    teacher: string;
+    totalCoursesAmount: number;
+    totalStudentsAmount: number;
+    ActiveSubjects: Array<Subject>;
+    InactiveSubjects: Array<Subject>;
+    CompletedSubjects: Array<Subject>;
+}
+
+export default function dashboard(props: DashboardProp) {
+
+    const { classes } = useStyles();
+
+    const [subjectShow, setSubjectShow] = useState(props.ActiveSubjects);
+    const [subjectState, setSubjectState] = useState('Active');
+    useEffect(()=> {
+        if (subjectState=="Active") {
+            setSubjectShow(props.ActiveSubjects);
+        }
+        else if (subjectState=="Inactive") {
+            setSubjectShow(props.InactiveSubjects);
+        }
+        else if (subjectState=="completed") {
+            setSubjectShow(props.CompletedSubjects);
+        }
+        console.log(`Subjects state: ${subjectState}`);
+        console.log(subjectShow);
+    }, [subjectState]);
+
+    const [coursePerformance, setCoursePerformance] = useState('');
+    useEffect(()=> {
+        console.log(`Course performance: ${coursePerformance}`);
+    })
+
     return (
         <>
             <NavBar />
-            <h1 className="text-2xl font-bold font-['Montserrat']">
-                Aj.Puun Dashboard
-            </h1>
+            <div style={{margin: 30, marginLeft: 50, marginRight: 80}}>
+                <h1 className="text-2xl font-bold font-['Montserrat']">
+                    {`Aj.${props.teacher} Dashboard`}
+                </h1>
+                
+                <div style={{display: 'flex'}}>
+                    <DashboardTotalCard title="total courses" amount={props.totalCoursesAmount}></DashboardTotalCard>
+
+                    <DashboardTotalCard title="total students" amount={props.totalStudentsAmount}></DashboardTotalCard>
+                </div>
+
+                <h1 style={{marginTop: 30}} className="text-2xl font-bold font-['Montserrat']">
+                    Courses
+                </h1>
+
+                <div style={{display: 'flex', marginTop: 20}}>
+                    <Chip.Group position="center">
+                        {/* <Chip classNames={classes} value="Active"
+                        onClick={() => setSubjectState('Active')}>
+                            <div className="text-2xs font-bold font-['Montserrat']">Active</div>
+                        </Chip>
+                        <Chip classNames={classes} value="Inactive"
+                        onClick={() => setSubjectState('Inactive')}>
+                            <div className="text-2xs font-bold font-['Montserrat']">Inctive</div>
+                        </Chip>
+                        <Chip classNames={classes} value="Completed"
+                        onClick={() => setSubjectState('Completed')}>
+                            <div className="text-2xs font-bold font-['Montserrat']">Completed</div>
+                        </Chip> */}
+                        
+                        <Chip classNames={classes} value='Active'
+                        onClick={() => setSubjectState('Active')}>Active</Chip>
+                        <Chip classNames={classes} value='Inactive'
+                        onClick={() => setSubjectState('Inactive')}>Inactive</Chip>
+                        <Chip classNames={classes} value='Completed'
+                        onClick={() => setSubjectState('Completed')}>Completed</Chip>
+
+                    </Chip.Group>
+                </div >
+
+                <div style={{marginTop: 10}} className="text-2xs font-bold font-['Montserrat']">
+                    {/* {`showing ${subjectShow.length}/${props.ActiveSubjects.length+
+                    props.InactiveSubjects.length+props.CompletedSubjects.length}`} */}
+                    showing
+                </div>
+
+                <div style={{display: 'flex', marginTop: 10, borderRadius: 20, backgroundColor: '#F2F2F2', height: 260}}>
+
+                </div>
+
+                <h1 style={{marginTop: 30}} className="text-2xl font-bold font-['Montserrat']">
+                    Courses Performance
+                </h1>
+
+                <div style={{display: 'flex', marginTop: 10}}>
+                    <NativeSelect
+                    // data={['Select course'].concat(subjectShow.map((subject)=> {
+                    //     return (
+                    //         subject.title
+                    //     )
+                    // }))}
+                    data={['Select course'].concat(['Calculus 1', 'Calculus 2', 'GenPhy 1'])}
+                    variant="filled"
+                    radius="xl"
+                    value={coursePerformance}
+                    onChange={(event) => setCoursePerformance(event.currentTarget.value)}
+                    />
+                </div>
+
+                <div style={{display: 'flex', marginTop: 20, borderRadius: 20, backgroundColor: '#F2F2F2', height: 500}}>
+
+                </div>
+
+
+                
+            </div>
         </>
     );
 }
